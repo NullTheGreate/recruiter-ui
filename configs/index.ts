ec37b60f132eb1fdf80eecf3b12218ffb84a35ed
theNull <>
2024-07-17 21:45:28 +0530

    corrected .env variable prefix

--- configs/index.ts
+++ configs/index.ts
@@ -1,3 +1,3 @@
 export const config = {
-    apiBaseUrl: process.env.API_BASE_URL
+    apiBaseUrl: process.env.NEXT_PUBLIC_API_BASE_URL
 }
--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-
-    async rewrites() {
+    async rewrites() {        
         return [
             {
                 source: '/api/users/:path*',
-                destination: `${process.env.API_BASE_URL}/api/users/:path*`,
+                destination: `${process.env.NEXT_PUBLIC_API_BASE_URL}/users/:path*`,
             },
         ];
     },
